1043125a806686aba792d9a24fb846cbdb57da0a
Your Name <you@example.com>
2022-05-27 00:32:37 +0300

    wrap venv create into start-process

--- Start.ps1
+++ Start.ps1
@@ -62,7 +62,7 @@ Start-Process @StartParams
 Write-MidleHost "Завантажено останню версію mhddos_proxy з GitHub" -Here -NoNewline
 
 Write-MidleHost "Створення віртуального оточення" -Here -NoNewline
-&"$PyPath\python.exe" -m virtualenv $VenvPath --quiet
+Start-Process -path "$PyPath\python.exe" -ArgumentList "-m virtualenv $VenvPath --quiet" -Wait -WindowStyle Hidden 
 
 Write-MidleHost "Активація віртуального оточення" -Here -NoNewline
 &"$VenvPath\Scripts\activate.ps1"
