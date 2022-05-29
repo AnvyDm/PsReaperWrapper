20ff48dfd2c50c9ab660652b211ff511fe6932f0
Your Name <you@example.com>
2022-05-29 23:31:45 +0300

    bugfix

--- Setup.ps1
+++ Setup.ps1
@@ -116,7 +116,7 @@ if (test-path "$RootPath\PsScripts") {
     Write-Host "`tPsUareaper було оновлено!"
 }
 else {
-    &"$RootPath\Git\cmd\git.exe" clone https://github.com/AnvyDm/PsUaReaper.git "$RootPath\PsScripts" --quiet
+    &"$RootPath\Git\cmd\git.exe" clone https://github.com/AnvyDm/PsUaReaper.git "$RootPath\PsScripts"
     Write-Host "`tPsUaReaper було встановлено!"
 }
 
